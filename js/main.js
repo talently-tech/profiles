fa48863fb5babfd4f7237020876a098fcacdb23b
ondoheer <ondoheer@gmail.com>
2019-10-27 11:49:00 -0500

    Added comments

--- js/main.js
+++ js/main.js
@@ -5,11 +5,17 @@ const initialProfiles = document.getElementsByClassName("c-profile");
 const selectors = document.querySelectorAll(".select select");
 // searchbar
 const searchBar = document.getElementById("search");
-// Deep clone all html elements
+// Deep clone all html elements so we can use it as base for our
+// filtering needs
 const allProfiles = Array.from(initialProfiles).map(profile =>
 	profile.cloneNode(true)
 );
 
+/**
+ * Uses the deep copy of the original profiles and then filteres it
+ * using functional methods so that the original list doesn't get destroyed.
+ * A new filtered list is returned on every filtering iteration
+ */
 const composedFilter = () => {
 	// current selectors values
 	const years = document.getElementById("years").value;
@@ -32,6 +38,10 @@ const composedFilter = () => {
 	return filteredProfiles;
 };
 
+/**
+ * replaces the current profiles with the filtered one
+ *
+ */
 const updateProfilesList = () => {
 	profilesContainer.innerHTML = "";
 	composedFilter().map(profile => profilesContainer.appendChild(profile));
@@ -41,4 +51,6 @@ const updateProfilesList = () => {
 for (let i = 0; i < selectors.length; i++) {
 	selectors[i].addEventListener("change", updateProfilesList);
 }
+
+// TODO throttle this if it starts showing up rendering delays
 searchBar.addEventListener("keyup", updateProfilesList);
