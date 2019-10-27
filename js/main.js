37341312e8ede5cbdefb2de67cf26638d6d8a41e
ondoheer <ondoheer@gmail.com>
2019-10-27 09:22:11 -0500

    Ready name search func

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 // Define interactive elements
 const profilesContainer = document.getElementById("profilesContainer");
 const initialProfiles = document.getElementsByClassName("c-profile");
+// selectors
+const selectors = document.querySelectorAll(".select select");
+// searchbar
+const searchBar = document.getElementById("search");
+// Deep clone all html elements
 const allProfiles = Array.from(initialProfiles).map(profile =>
 	profile.cloneNode(true)
 );
-// selectors
-const selectors = document.querySelectorAll(".select select");
 
 const composedFilter = e => {
 	// current selectors values
 	const years = document.getElementById("years").value;
 	const language = document.getElementById("programming_languages").value;
+	const searchText = searchBar.value.toLowerCase();
+	console.log(searchText === "");
 	//filter by age
 	const filteredProfiles = allProfiles
 		.filter(profile => profile.dataset.years >= years)
@@ -18,6 +23,11 @@ const composedFilter = e => {
 			profile =>
 				language === "all" ||
 				profile.dataset.languages.indexOf(language) !== -1
+		)
+		.filter(
+			profile =>
+				searchText === "" ||
+				profile.dataset.name.toLowerCase().indexOf(searchText) !== -1
 		);
 	return filteredProfiles;
 };
@@ -27,6 +37,8 @@ const updateProfilesList = e => {
 	composedFilter(e).map(profile => profilesContainer.appendChild(profile));
 };
 
+// Add event listeners
 for (let i = 0; i < selectors.length; i++) {
 	selectors[i].addEventListener("change", updateProfilesList);
 }
+searchBar.addEventListener("keyup", updateProfilesList);
